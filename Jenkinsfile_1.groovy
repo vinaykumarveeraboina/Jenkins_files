dffdedb87ff7cb625645bfdd291f8590108ba1e0
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-24 22:52:55 +0530

    added new line

--- Jenkinsfile_1.groovy
+++ Jenkinsfile_1.groovy
@@ -5,11 +5,17 @@ comment multiple lines */
 
 
 pipeline{
-agent any    // we can expecute a pipeline or stage with any agent available 
+agent any    // we can expecute a pipeline or stage with any agent available
+
+ /*agent 
+   label = mvnnode  */    //ideally this is a string pased , which inform our jib to run on particular stage 
+
+
 stages{
-    stage('First stage')  //this can be user friendly but need to be meaning full for task 
+    stage('First stage') { //this can be user friendly but need to be meaning full for task 
     steps{
         echo " example for using jenkins files "
     }
+}
 }
 }
