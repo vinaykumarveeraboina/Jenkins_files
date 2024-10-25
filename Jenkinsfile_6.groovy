d4d05cd8c4e54033f6aaeb5d1660ff8546d65d7a
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-25 19:25:33 +0530

    timeout

---
+++ Jenkinsfile_6.groovy
@@ -0,0 +1,15 @@
+//time out example ,
+//here we are keeping 60 seconds sleep, but time out is 2 seconds , step will wait for only 2 seconds
+
+pipeline{
+    agent any
+    stages{
+        stage('build'){
+            steps{
+                timeout(time :2,unit :SECONDS)
+                echo "sleeping for 60 seconds"
+                sleep 60
+            }
+        }
+    }
+}
