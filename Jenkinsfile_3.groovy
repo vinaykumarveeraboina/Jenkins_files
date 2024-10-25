fc2aed027e740ceed06a0b5c2900e5fcb8edd23f
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-25 14:05:32 +0530

    node agent

---
+++ Jenkinsfile_3.groovy
@@ -0,0 +1,30 @@
+//comment types
+
+/*
+comment multiple lines */
+
+
+//agent { lebel 'label name '} -- will run on mvn slave 
+
+pipeline{
+agent  none  // this applies to entire pipleline     
+stages{
+    stage('First stage') {
+        agent{   // this is specific to stage 
+            node {
+                label mvn-slave
+                customworkspace '/home/jslave/agnettest'
+            }
+        }
+    steps{
+        echo " example for using jenkins files and node agent "
+        sh 'hostname -i'
+        sh 'pwd'
+        sh 'cat agnet.txtfile'
+
+      
+    }
+}
+}
+}
+
