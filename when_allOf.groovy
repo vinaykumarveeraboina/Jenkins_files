a81c75d573aff29a47a1826774bb8cd9787e6060
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 13:43:02 +0530

    anyoff

--- when_allOf.groovy
+++ when_allOf.groovy
@@ -1,4 +1,4 @@
-pipeline{
+/*pipeline{
     agent any
     environment{
         DEPLOY_to = 'develop'
@@ -30,5 +30,40 @@ pipeline{
                 }
         }
      }
+}*/
+
+
+
+pipeline{
+    agent any
+    environment{
+        DEPLOY_to = 'develop'
+    }
+    stages{
+        
+        stage ('deploy to dev'){  //we are saying excute this job only in develop (job)
+            when {
+                expression {
+                    BRANCH_NAME == "develop" 
+                }
+              }  
+              steps{
+                echo "deploying to develop branch "
+              }
+          
+            }
+       
+        stage ('testing all off condition') {
+            when {
+                anyOf { //any of the below condition can be satisfied 
+                   branch 'develop' 
+                   environment name :'DEPLOY_to', value : 'develop'
+                } 
+                }
+             steps{
+                  echo " deploying to '${BRANCH_NAME}'"
+                  
+                }
+        }
+     }
 }
-  
