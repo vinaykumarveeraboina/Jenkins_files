4c4ab67a19f0ae5cfbfaf78d99e22d1130a09192
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 10:55:41 +0530

    condition

---
+++ Conditions.groovy
@@ -0,0 +1,16 @@
+pipeline{
+    agent any
+    environment{
+        Deploy_to= 'prod'
+    }
+    stages{
+        stage ('deploy') {
+            when {environmnet name : 'DEPLOY_to', value : 'prod'
+             steps{
+                  echo " deploying "
+                  
+                }
+        }
+        }
+    }
+}
