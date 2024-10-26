3babe9db75af412df21498ebe39696f701281d40
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 18:36:44 +0530

    gugyth

--- parallel.groovy
+++ parallel.groovy
@@ -2,7 +2,7 @@ pipeline{
     agent any 
     stages{
         stage('running parallel jobs'){
-            failFast true
+            failFast false
         parallel{
             stage('sonar')
             {
