6f0c55eb331361e53ba01b7c28b210fd0718d82b
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 20:43:04 +0530

    params

--- parameters.groovy
+++ parameters.groovy
@@ -7,12 +7,19 @@ parameters {
 //choice
 string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
 //real time we can give branch name 
-string(name: 'Branch', defaultValue: 'main', description: 'Who brnch should I deploy?')
+//string(name: 'Branch', defaultValue: 'main', description: 'Who brnch should I deploy?')
+choice(
+     
+     choice(name: 'ENVironmnet', choices: ['main', 'dev', 'stage' 'prod'], description: 'Pick something')
+
+)
 }
 stages{
     stage('param stage'){
         steps{
            echo "Hello ${params.PERSON}"   //person is a variable of type string and take the string input and print it 
+           echo "deployed on ${param.ENVironmnet}"
+
         }
     }
 }
