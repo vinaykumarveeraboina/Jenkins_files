a2d5a2876746bfa68aa7ca5e924f9caf7ce6950d
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 19:03:48 +0530

    input

---
+++ input.groovy
@@ -0,0 +1,12 @@
+pipeline{
+    agent any 
+    stages{
+        stage('input prompt'){
+            steps{
+                timeout(time :300, unit:'SECONDS')
+                input messages : 'are you building the application' ok: 'yes' submitter: 'vinay kumar veeraboina'
+            }
+               echo "building application "
+         }
+    }
+}
