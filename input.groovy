3d54ee90a906d8c2821d085de767a99016ad6eac
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 19:08:03 +0530

    ddd

--- input.groovy
+++ input.groovy
@@ -3,10 +3,11 @@ pipeline{
     stages{
         stage('input prompt'){
             steps{
-                timeout(time :300, unit:'SECONDS')
+                timeout(time :300, unit:'SECONDS'){
                 input messages : 'are you building the application', ok: 'yes', submitter: 'vinay kumar veeraboina'
             }
                echo "building application "
          }
     }
+}
 }
