5b2eaa83171b8a543fd458f168c3af29c02b3a2a
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 11:16:24 +0530

    new way of when block

--- Conditions.groovy
+++ Conditions.groovy
@@ -1,4 +1,4 @@
-pipeline{
+/*pipeline{
     agent any
     environment{
         Deploy_to= 'unknown'
@@ -6,7 +6,32 @@ pipeline{
     stages{
         stage ('deploy') {
             when {
-                environment name : 'DEPLOY_to', value : 'prod'
+                environment name : 'DEPLOY_to', value : 'prod' //when name =value execute it 
+                }
+             steps{
+                  echo " deploying "
+                  
+                }
+        }
+        }
+    }
+    */
+
+//another way of writing the code 
+
+pipeline{
+    agent any
+    environment{
+        Deploy_to= 'prod'
+    }
+    stages{
+        stage ('deploy') {
+            when {
+                equals expected : 'prod', actual: "${Deploy_to}"
+                /* Here we are expecting prod and it is comparing to the value in depoy_to variable 
+                which we called in actual block actual: "${Deploy_to}
+                 when expected is equal to actual , excute the the when block 
+                 */
                 }
              steps{
                   echo " deploying "
