efe9fbc07ef9461ff465d6cb01c6939c550eb9cf
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-10-26 11:26:50 +0530

    not condition example

--- Conditions.groovy
+++ Conditions.groovy
@@ -19,7 +19,7 @@
 
 //another way of writing the code 
 
-pipeline{
+/* pipeline{
     agent any
     environment{
         Deploy_to= 'prod'
@@ -31,10 +31,33 @@ pipeline{
                 /* Here we are expecting prod and it is comparing to the value in depoy_to variable 
                 which we called in actual block actual: "${Deploy_to}
                  when expected is equal to actual , excute the the when block 
-                 */
+                
                 }
              steps{
                   echo " deploying "
+                  
+                }
+        }
+        }
+    }
+    */
+
+//not condition 
+
+    pipeline{
+    agent any
+    environment{
+        Deploy_to= 'prod'
+    }
+    stages{
+        stage ('deploy') {
+            when {
+                not{
+                equals expected : 'prod', actual: "${Deploy_to}"
+                }
+                }
+             steps{
+                  echo " deploying when condition "
                   
                 }
         }
